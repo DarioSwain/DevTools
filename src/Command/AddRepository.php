805214bdb79b8276feda2f3d15f6aa972a961237
Ilya Pokamestov <dario_swain@yahoo.com>
2017-04-06 14:08:20 +0000

    Initial

---
+++ src/Command/AddRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DS\DevTools\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+class AddRepository  extends Command
+{
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ...
+    }
+}
